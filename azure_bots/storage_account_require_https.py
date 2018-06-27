e9d6f2ddbbc89b27e310fee44ff3621a0cb912c6
alpalwal <alex@dome9.com>
2018-06-27 12:39:34 -0700

    first push

---
+++ azure_bots/storage_account_require_https.py
@@ -0,0 +1,39 @@
+'''
+storage_account_require_https
+Usage: AUTO: storage_account_require_https
+'''
+
+import traceback
+from packages.msrestazure.azure_exceptions import CloudError
+
+from azure.mgmt.storage.models import StorageAccountUpdateParameters
+
+def run_action(azure_client,rule,entity,params):
+  text_output = ""
+
+  resource_group_name = entity['resourceGroup']['name']
+  storage_account_name = entity['name']
+
+  try:
+    text_output = "Updating setting for enabling HTTPS only traffic on Storage Account: %s \n" % storage_account_name
+
+    # Update storage account
+    print('Update storage account')
+    storage_account = azure_client['storage'].storage_accounts.update(
+        resource_group_name, 
+        storage_account_name,
+        StorageAccountUpdateParameters(
+            enable_https_traffic_only=True
+        )  
+    )
+
+  except CloudError:
+    text_output =  text_output + 'Operation failed:' + traceback.format_exc()
+      
+  else:
+    text_output = text_output +  "Storage account: %s successfully updated\n" % storage_account_name
+
+  return text_output
+
+
+
